d63fb90f35644446d1fc786f407b894016706332
jambrocode <iqbalkhangori850@gmail.com>
2023-02-05 14:29:23 +0530

    New version V1.0.2

--- index.js
+++ index.js
@@ -2,56 +2,73 @@
 const fs = require('fs');
 const path = require('path');
 const args = process.argv.slice(2);
-const directory=args[0]
+var directory=args[0]
 var extension=args[1]
+var ignoreDirs = ['node_modules'];
+
+var ignore=false
+args.forEach((element,i) => {
+    if(element==="-ignore"){
+       ignore=true
+    }
+    else if(ignore){
+        ignoreDirs.push(element);
+    }
+});
+
 if(args.length===0 || args===null){
-    console.log("!!!!please provide the directory and file extension i.e .js ");
-    return
+   directory="./";
+   extension=".js";
 }
 
 if(extension===null)
 {
-    console.log('Using default .js extension as no proper extension provided!');
+   extension=".js"
 }
-fs.readdir(directory, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  if(files.length===1){
-    if(path.extname(files[0])===extension)
- {fix_file(files[0],directory);}
- else 
-   {console.log(`No files found with given ${extension} extension in ${directory}`);}
-  }
-  else{
-  files.forEach(file => {
-    if (path.extname(file) === extension) {
-      fix_file(file,directory);
-    }
+// console.log(extension, directory)
+//V1.0.2
+const search_file=(dir,level)=> {
+  fs.readdirSync(dir).forEach(file => {
+      const filepath = path.join(dir, file);
+      if (fs.lstatSync(filepath).isDirectory() && !ignoreDirs.includes(file)) {
+          search_file(filepath,level+1);
+      } else if (path.extname(file) === extension && !ignoreDirs.includes(file)) {
+        console.log(`${'  '.repeat(level)}${filepath}`);
+        fix_file(filepath);
+      }
   });
 }
-});
 
+const fix_file=(filepath)=>{
 
-const fix_file=async (file,directory)=>{
-
-    fs.readFile(path.join(directory, file), 'utf8', (err, data) => {
+    fs.readFile(filepath, 'utf8', (err, data) => {
         if (err) {
           console.error(err);
           return;
         }
 
-        const commented = data.replace(/console\.log\(.*\);/g, '// $&');
-        fs.writeFile(path.join(directory, file), commented, 'utf8', err => {
+        const commented = data.replace(/console\.log\(.*\);/g, '// $&').replace(/console\.log\(.*\)/g, '// $&');
+        fs.writeFile(filepath, commented, 'utf8', err => {
           if (err) {
             console.error(err);
             return;
           }
-
-          console.log(`Commented console.log() statements in ${file}.`);
-          console.log("Happ production :)")
+         console.log(`fixed file ${filepath}`);
+          return;
         });
       });
 
 }
+
+
+
+// V1.0.1
+fs.readdir(directory, async (err, files) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  else{
+   search_file(directory,0);
+  }
+});
